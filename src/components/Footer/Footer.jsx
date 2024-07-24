b8a5f8b3a9e90fcfcfcf61d6207e8f54db95ff21
btrindadefm <btrindade172@gmail>
2024-07-24 01:12:13 -0300

    feat: adicionando link para meus perfis

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,10 +16,26 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className='social'>
-        <a className='linkedin' href=""><FaLinkedinIn /></a>
+        <a
+          className='linkedin'
+          href="https://www.linkedin.com/in/bruno-trindade-883b03300/"
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='LinkedIn Profile'
+          >
+          <FaLinkedinIn />
+        </a>
       </div>
       <div className='social'>
-        <a className='github' href=""><FaGithub /></a>
+        <a
+          className='github'
+          href="https://github.com/brunotrindadefm"
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='GitHub Profile'
+          >
+          <FaGithub />
+        </a>
       </div>
     </footer>
   )
