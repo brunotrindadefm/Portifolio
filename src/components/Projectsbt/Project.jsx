4180d23be8e337199cd9b5590b7eea586cd7db6a
btrindadefm <btrindade172@gmail>
2024-07-25 10:58:52 -0300

    feat: adicionando info para os projetos

--- src/components/Projectsbt/Project.jsx
+++ src/components/Projectsbt/Project.jsx
@@ -1,19 +1,90 @@
 import { FaHtml5, FaCss3Alt, FaReact, FaBootstrap, FaSass } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
 
-const Project = () => {
+import { useState } from "react";
 
+const Project = ({ lang }) => {
+
+    const [selectedProjectIndex, setSelectedProjectIndex] = useState(null);
+       
     const projects = [
-        { name: 'Portifólio', image: '/portifolio.png', technologies: [<FaReact key="react" />, <FaSass key='sass' />] },
-        { name: 'API Movies', image: '/apiMovies.png', technologies: [<FaReact key="react" />, <FaCss3Alt key='css' />] },
-        { name: 'API Clima', image: '/apiClima.png', technologies: [<FaReact key="react" />, <FaCss3Alt key='css' />] },
-        { name: 'API Spotify', image: '/apiSpotify.png', technologies: [<FaReact key="react" />, <FaCss3Alt key='css' />] },
-        { name: 'Tic Tac Toe', image: '/TicTacToe.png', technologies: [<FaReact key="react" />, <FaCss3Alt key='css' />] },
-        { name: 'API Cep', image: '/apiCep.png', technologies: [<FaReact key="react" />, <FaCss3Alt key='css' />] },
-        { name: 'Calculadora', image: '/calculadora.png', technologies: [<FaReact key="react" />, <FaCss3Alt key='css' />] },
-        { name: 'Treinamento Mira', image: '/TreinamentoAim.png', technologies: [<FaHtml5 key="html" />, <FaCss3Alt key="css" />, <IoLogoJavascript key='js' />] },
-        { name: 'Calculadora de IMC', image: '/calculadoradeImc.png', technologies: [<FaHtml5 key="html" />, <FaCss3Alt key="css" />, <IoLogoJavascript key='js' />, <FaBootstrap key='bootstrap' />] }
+        {
+            name: 'API Movies',
+            image: '/apiMovies.png',
+            technologies: [<FaReact key="react" />, <FaSass key='sass' />],
+            description: {
+                en: 'A project about movies data fetching using a public API.',
+                pt: 'Um projeto sobre busca de dados de filmes usando uma API pública.'
+            }
+        },
+        {
+            name: 'API Clima',
+            image: '/apiClima.png',
+            technologies: [<FaReact key="react" />, <FaCss3Alt key="css" />],
+            description: {
+                en: 'A weather application using a weather API.',
+                pt: 'Um aplicativo de clima usando uma API de clima.'
+            }
+        },
+        {
+            name: 'API Spotify',
+            image: '/apiSpotify.png',
+            technologies: [<FaReact key="react" />, <FaCss3Alt key="css" />],
+            description: {
+                en: 'A music streaming project using Spotify API.',
+                pt: 'Um projeto de streaming de música usando a API do Spotify.'
+            }
+        },
+        {
+            name: 'Tic Tac Toe',
+            image: '/TicTacToe.png',
+            technologies: [<FaReact key="react" />, <FaCss3Alt key="css" />],
+            description: {
+                en: 'A simple Tic Tac Toe game.',
+                pt: 'Um jogo simples de Tic Tac Toe.'
+            }
+        },
+        {
+            name: 'API Cep',
+            image: '/apiCep.png',
+            technologies: [<FaReact key="react" />, <FaCss3Alt key="css" />],
+            description: {
+                en: 'A project for fetching and displaying Brazilian postal codes.',
+                pt: 'Um projeto para buscar e exibir códigos postais brasileiros.'
+            }
+        },
+        {
+            name: 'Calculadora',
+            image: '/calculadora.png',
+            technologies: [<FaReact key="react" />, <FaCss3Alt key="css" />],
+            description: {
+                en: 'A basic calculator project.',
+                pt: 'Um projeto de calculadora básica.'
+            }
+        },
+        {
+            name: 'Treinamento Mira',
+            image: '/TreinamentoAim.png',
+            technologies: [<FaHtml5 key="html" />, <FaCss3Alt key="css" />],
+            description: {
+                en: 'An aim training tool for gamers.',
+                pt: 'Uma ferramenta de treinamento de mira para jogadores.'
+            }
+        },
+        {
+            name: 'Calculadora de IMC',
+            image: '/calculadoradeImc.png',
+            technologies: [<FaHtml5 key="html" />, <FaCss3Alt key="css" />, <IoLogoJavascript key='js' />, <FaBootstrap key='bootstrap' />],
+            description: {
+                en: 'A BMI calculator project.',
+                pt: 'Um projeto de calculadora de IMC.'
+            }
+        }
     ];
+
+    const handleInfoClick = (index) => {
+        setSelectedProjectIndex(index === selectedProjectIndex ? null : index);
+    }
 
     return (
         <div className='card-projects'>
@@ -26,7 +97,10 @@ const Project = () => {
                             <span key={i}>{Icon}</span>
                         ))}
                     </div>
-                    <button className='btn' aria-label={`More info about project ${index + 1}`}>info</button>
+                    <button className='btn' onClick={() => handleInfoClick(index)} aria-label={`More info about project ${index + 1}`}>Info</button>
+                    {selectedProjectIndex === index && (
+                        <p>{lang ? project.description.pt : project.description.en}</p>
+                    )}
                 </div>
             ))}
         </div>
--- src/components/Projectsbt/Projectsbt.jsx
+++ src/components/Projectsbt/Projectsbt.jsx
@@ -1,18 +1,27 @@
+import { useEffect, useState } from 'react'
 import Project from './Project'
 import './Projects.css'
 
 const Projectsbt = ({ lang }) => {
 
+    const [projectsTitle, setProjectsTitle] = useState('')
 
+    useEffect(() => {
+        if(lang) {
+            setProjectsTitle('Meus Projetos')
+        } else {
+            setProjectsTitle('My Projects')
+        }
+    },[lang])
 
     return (
         <div className='projects-container'>
             <div className='projects-tit'>
-                <h2>My projects</h2>
+                <h2>{projectsTitle}</h2>
                 <div></div>
             </div>
 
-           <Project />
+           <Project lang={lang} />
 
         </div>
     )
