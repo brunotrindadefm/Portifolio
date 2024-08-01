5c2df5fbf688a1812861d61ef578fd93ff9ac0a1
btrindadefm <btrindade172@gmail>
2024-08-01 16:37:31 -0300

    feat: adicionando funcionalidade de baixar o currículo ao cliclar no botão de baixar

--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -7,7 +7,9 @@ import 'aos/dist/aos.css';
 
 import { FaHandPeace } from "react-icons/fa";
 
-const Hello = ({lang}) => {
+import profileBruno from '/public/ProfileBruno.pdf'
+
+const Hello = ({ lang }) => {
 
     const [about, setAbout] = useState('')
     const [name, setName] = useState('')
@@ -15,11 +17,11 @@ const Hello = ({lang}) => {
     const [btn, setBtn] = useState('')
 
     useEffect(() => {
-        if(!lang) {
-        setBtn("Download CV")
-        setSaudacoes('Hello')
-        setName("I'm Bruno")
-        setAbout('Dedicated Computer Science student with a passion for technology and problem solving. Committed to seeking opportunities that allow me to apply and expand my knowledge. I am looking for internship opportunities that allow me to develop my practical skills and contribute to innovative solutions in the technology area.')
+        if (!lang) {
+            setBtn("Download CV")
+            setSaudacoes('Hello')
+            setName("I'm Bruno")
+            setAbout('Dedicated Computer Science student with a passion for technology and problem solving. Committed to seeking opportunities that allow me to apply and expand my knowledge. I am looking for internship opportunities that allow me to develop my practical skills and contribute to innovative solutions in the technology area.')
         } else {
             setBtn("Baixar CV")
             setSaudacoes('Olá')
@@ -30,11 +32,18 @@ const Hello = ({lang}) => {
     }, [lang])
 
     return (
-        <div data-aos="fade-up"  className='hello'>
+        <div data-aos="fade-up" className='hello'>
             <h5 data-aos="fade-left" data-aos-duration="1800"><span>{saudacoes} <FaHandPeace /></span></h5>
             <h1 data-aos="fade-up" data-aos-duration="1500">{name}</h1>
             <p data-aos="fade-down" data-aos-duration="2000">{about}</p>
-            <button data-aos="fade-left" data-aos-duration="1800" className='btn'>{btn}</button>
+            <a
+                href={profileBruno}
+                download={profileBruno}
+                data-aos="fade-left"
+                data-aos-duration="1800"
+            >
+                <button className='btn'>{btn}</button>
+            </a>
         </div>
     )
 }
