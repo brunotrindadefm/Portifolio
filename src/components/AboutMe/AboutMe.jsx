6c59f673e1ac46c51a285f0fcea6238cb58cc6e6
btrindadefm <btrindade172@gmail>
2024-08-03 12:35:47 -0300

    feat: little changes

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -21,20 +21,20 @@ const AboutMe = ({ lang }) => {
 
     useEffect(() => {
         if (!lang) {
-            setTitle("I'm Bruno")
-            setSubtitle('Who is Bruno?')
-            setText1('Bruno is a Computer Science student in the second semester of his degree. With a natural curiosity for technology and a passion for solving problems, he is looking for his first internship. Bruno is eager to apply his theoretical skills in a professional environment, learn from experts and contribute to innovative projects.')
-            setText2('Currently in the second semester of Computer Science, Bruno is preparing to enter the job market with his first internship. He has dedicated himself to studying programming and software development, and now wants to complement his academic training with practical experiences. Bruno is excited to face new challenges and help the team achieve its goals.')
-            setText3("In the second semester of his Computer Science degree, Bruno is ready to take the next step in his professional journey. He is looking for an internship that allows him to utilize his programming and development skills, while learning from hands-on experience and contributing to the team's success. Bruno is eager to face challenges and grow in the field of technology.")
-            setText4('Bruno, who is in the second semester of Computer Science, is looking for his first internship to apply the knowledge acquired in his classes and explore the world of technology in a practical way. With a proactive approach and a great desire to learn, he is ready to dedicate himself to challenging projects and grow as a professional in the computing field.')
+            setSubtitle("I'm Bruno")
+            setTitle('Who is Bruno?')
+            setText1('In the second semester of the Computer Science program, I am seeking my first internship. With a natural curiosity for technology and a passion for solving problems, I am eager to apply my theoretical skills in a professional environment, learn from experts, and contribute to innovative projects.')
+            setText2('Preparing to enter the job market with my first internship, I have dedicated myself to studying programming and software development. I am looking to complement my academic training with practical experiences and am excited to face new challenges while helping the team achieve its goals.')
+            setText3("Ready to take the next step in my professional journey, I am looking for an internship that allows me to utilize my programming and development skills while learning from hands-on experience and contributing to the team's success. I am eager to face challenges and grow in the field of technology.")
+            setText4('I am seeking my first internship to apply the knowledge acquired in my classes and explore the world of technology in a practical way. With a proactive approach and a great desire to learn, I am prepared to dedicate myself to challenging projects and grow as a professional in the computing field.')
             setSkills('Skills')
         } else {
-            setTitle("Eu sou Bruno")
-            setSubtitle('Quem é Bruno?')
-            setText1('Bruno é um estudante de Ciência da Computação no segundo semestre de sua graduação. Com uma curiosidade natural por tecnologia e uma paixão por resolver problemas, ele está em busca de seu primeiro estágio. Bruno está ansioso para aplicar suas habilidades teóricas em um ambiente profissional, aprender com especialistas e contribuir para projetos inovadores.')
-            setText2('Atualmente no segundo semestre de Ciência da Computação, Bruno está se preparando para entrar no mercado de trabalho com seu primeiro estágio. Ele tem se dedicado a estudar programação e desenvolvimento de software, e agora quer complementar sua formação acadêmica com experiências práticas. Bruno está empolgado para enfrentar novos desafios e ajudar a equipe a alcançar seus objetivos.')
-            setText3('No segundo semestre de sua graduação em Ciência da Computação, Bruno está pronto para dar o próximo passo em sua jornada profissional. Ele busca um estágio que lhe permita utilizar suas habilidades em programação e desenvolvimento, enquanto aprende com a experiência prática e contribui para o sucesso da equipe. Bruno está ansioso para enfrentar desafios e crescer no campo da tecnologia.')
-            setText4('Bruno, que está no segundo semestre de Ciência da Computação, está em busca de seu primeiro estágio para aplicar o conhecimento adquirido em suas aulas e explorar o mundo da tecnologia de maneira prática. Com uma abordagem proativa e uma grande vontade de aprender, ele está pronto para se dedicar a projetos desafiadores e crescer como profissional no campo da computação.')
+            setSubtitle("Eu sou Bruno")
+            setTitle('Quem é Bruno?')
+            setText1('No segundo semestre do curso de Ciência da Computação, busco meu primeiro estágio. Com uma curiosidade natural por tecnologia e uma paixão por resolver problemas, tenho vontade de aplicar minhas habilidades teóricas em um ambiente profissional, aprender com especialistas e contribuir para projetos inovadores.')
+            setText2('Com a preparação para entrar no mercado de trabalho com o primeiro estágio, tenho me dedicado ao estudo de programação e desenvolvimento de software. O objetivo é complementar a formação acadêmica com experiências práticas. Estou empolgado para enfrentar novos desafios e ajudar a equipe a alcançar seus objetivos.')
+            setText3('Pronto para dar o próximo passo na jornada profissional, procuro um estágio que permita utilizar minhas habilidades em programação e desenvolvimento, ao mesmo tempo em que aprendo com a experiência prática e contribuo para o sucesso da equipe. A expectativa é enfrentar desafios e crescer na área de tecnologia.')
+            setText4('Busco meu primeiro estágio para aplicar os conhecimentos adquiridos nas aulas e explorar a tecnologia de maneira prática. Com uma abordagem proativa e um grande desejo de aprender, estou preparado para me dedicar a projetos desafiadores e crescer como profissional na área de computação.')
             setSkills('Habilidades')
         }
     }, [lang])
